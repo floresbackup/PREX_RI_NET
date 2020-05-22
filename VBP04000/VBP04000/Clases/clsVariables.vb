c78684b95fcae2e3ce0ad3679055206e812ec60b
Ricardo Flores <36236315+floresbackup@users.noreply.github.com>
2020-05-22 00:08:08 -0300

    clsVariables

---
+++ VBP04000/VBP04000/Clases/clsVariables.vb
@@ -0,0 +1,12 @@
+Public Class clsVariables
+
+   Public CodCon As Long
+   Public Orden As Integer
+   Public Nombre As String
+   Public Tipo As Integer
+   Public Titulo As String
+   Public Help As Integer
+   Public HelpQuery As String
+   Public Key As String
+
+End Class
